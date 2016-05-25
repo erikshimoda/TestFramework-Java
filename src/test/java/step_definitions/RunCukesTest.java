77f8ad68279debfbc1805f82cd452c590592ad59
erikshimoda <erikshimoda@gmail.com>
2016-05-25 18:09:44 -0300

    Adição anotação @testeAceitação

--- src/test/java/step_definitions/RunCukesTest.java
+++ src/test/java/step_definitions/RunCukesTest.java
@@ -5,13 +5,14 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:features",
-		plugin = {"pretty", "html:target/cucumber-html-report","json:cucumber.json"},
-		tags = {}
-		)
-public class RunCukesTest{
-	
+		features = "classpath:features"
+		, plugin = { "pretty", "html:target/cucumber-html-report", "json:cucumber.json" }
+		, monochrome = true
+		, tags = { "@testeAceitacao" }
+)
+
+public class RunCukesTest {
+
 }
