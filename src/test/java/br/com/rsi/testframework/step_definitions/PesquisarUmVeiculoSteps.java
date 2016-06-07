ae48399e25060a171b6213599806e8e79ab80213
erik <erikshimoda@gmail.com>
2016-06-07 00:03:59 -0300

    update

---
+++ src/test/java/br/com/rsi/testframework/step_definitions/PesquisarUmVeiculoSteps.java
@@ -0,0 +1,63 @@
+package br.com.rsi.testframework.step_definitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import br.com.rsi.testframework.helpers.Log;
+import br.com.rsi.testframework.modules.PreencherDadosFiltroVeiculo;
+import br.com.rsi.testframework.pageobjects.HomePage;
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+public class PesquisarUmVeiculoSteps {
+	public WebDriver driver;
+
+	public PesquisarUmVeiculoSteps() {
+		Log.iniciadoCasoDeTeste("Pesquisar um Veiculo no Site da BV");
+		driver = Hooks.driver;
+	}
+
+	@Quando("eu pesquiso pelo modelo \"([^\"]*)\"$")
+	public void eu_pequiso_pelo_modelo(String modelo) throws Throwable {
+		PageFactory.initElements(driver, HomePage.class);
+		PreencherDadosFiltroVeiculo.InformarModelo(driver, modelo);
+	}
+
+	@E("informo o ano inicial de \"([^\"]*)\"$")
+	public void informo_o_ano_inicial(String anoInicio) throws Throwable {
+		PageFactory.initElements(driver, HomePage.class);
+		PreencherDadosFiltroVeiculo.InformarAnoInicio(driver, anoInicio);
+	}
+
+	@E("informo o ano final de \"([^\"]*)\"$")
+	public void informo_o_ano_final(String anoFim) throws Throwable {
+		PageFactory.initElements(driver, HomePage.class);
+		PreencherDadosFiltroVeiculo.InformarAnoFim(driver, anoFim);
+	}
+
+	@Quando("^clico no Buscar$")
+	public void clico_no_Buscar() throws Throwable {
+		PageFactory.initElements(driver, HomePage.class);
+		PreencherDadosFiltroVeiculo.ClicarBuscar(driver);
+	}
+
+	@Entao("^eu visualizo os detalhes do veiculo$")
+	public void eu_visualizo_os_detalhes_do_veiculo() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Entao("^eu visualizo uma lista de modelos de veiculos$")
+	public void eu_visualizo_uma_lista_de_modelos_de_veículos() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Entao("^eu clico para Visualizar os Detalhes$")
+	public void eu_clico_para_Visualizar_os_Detalhes() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+}
